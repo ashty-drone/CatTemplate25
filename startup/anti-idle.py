d01bf11918407869f6cc68aad838a202f3f5868d
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-02-02 02:07:19 +0530

    Update anti-idle.py

--- startup/anti-idle.py
+++ startup/anti-idle.py
@@ -1,9 +1,8 @@
 import asyncio
-from logging import getLogger
 
 async def continuous_print():
     while True:
-        getLogger("AliveCheck").info('I am Alive!!')
+        print('I am Alive!!')
         await asyncio.sleep(300)
     return False
 
