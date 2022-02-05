954c593ffcef9eff324e609ee5d5dc21340122ab
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-02-05 18:18:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ wget -q https://github.com/ashty-drone/nekopack/archive/master.zip
 unzip -qq *.zip
 rm -f *.zip
 mv * catuserbot
-mv catuserbot ../
+mv catuserbot ../ && cd ..
 rm -r -f tempdir
 cd catuserbot
 bash start
